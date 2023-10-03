33fc2af0ceed53d0329762357e1dd9f01bebad6c
Verviam <gaoevan37@gmail.com>
2023-10-03 17:26:17 -0400

    blah bal

---
+++ index.js
@@ -0,0 +1,11 @@
+const x = {
+    name: prompt ('What is your name?')
+    height: prompt('height in cm')
+    weight: prompt('Weight in kg')
+}
+
+
+function BMI(height, weight) {
+    return (weight / height )(weight/height) * 703
+}
+
