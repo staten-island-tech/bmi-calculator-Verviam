3d25817314837dc4259af22b499fadb4082884ba
Evan Gao <gaoevan37@gmail.com>
2023-10-04 09:19:26 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const x = {
-    name: prompt ('What is your name?')
-    height: prompt('height in cm')
-    weight: prompt('Weight in kg')
+unitSystem = prompt('Choose to use imperial or metric system (i/m)')
+
+if (unitSystem === 'i') {
+    username= prompt ('What is your name?');
+    height= prompt('Enter your height in inches');
+    weight= prompt('Enter your weight in lbs');
+    BMI = iFindBMI(height, weight);
+    console.log('Hi' + ' ' + username + ' ' + 'your BMI is' + ' ' + BMI);
 }
 
-
-function BMI(height, weight) {
-    return (weight / height )(weight/height) * 703
+    else if (unitSystem === 'm') {
+        username= prompt ('What is your name?');
+        height= prompt('Enter your height in cm');
+        weight= prompt('Enter your weight in kg');
+        BMI = mFindBMI(height, weight);
+        console.log('Hi' + ' ' + username + ' ' + 'your BMI is' + ' ' + BMI);
 }
 
+function iFindBMI(height, weight) {
+    return (weight / height ** 2) * 703
+}
+
+function mFindBMI(height, weight) {
+    return (weight / height ** 2) * 10000
+}
+
+if (BMI <= 18.5) {
+    console.log('You are underweight');
+}
+    else if (BMI <= 24.9) {
+        console.log('You are healthy');
+    }
+    else if (BMI >= 30) {
+        console.log('You are obese');
+    }
